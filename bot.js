46d45a334e1882b9317bd23472b3bd7a6e3a0d3d
Kirill Vitchenko <vitchenko.kirill@gmail.com>
2023-03-19 18:14:11 +0200

    init

---
+++ bot.js
@@ -0,0 +1,48 @@
+const { Telegraf } = require("telegraf");
+const dotenv = require("dotenv");
+const moment = require("moment");
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const interval = 10000;
+
+const getDaysLeft = () => {
+  const currentDate = moment();
+  const targetDate = moment("2023-08-01", "YYYY-MM-DD");
+
+  return targetDate.diff(currentDate, "days");
+};
+
+const sendStandardMessage = (ctx) => {
+  const daysLeft = getDaysLeft();
+  let message = `До отпуска осталось ${daysLeft} дней. Держитесь!`;
+
+  if (daysLeft <= 0) {
+    message = `Отпуск блеать!`;
+  }
+
+  bot.telegram.sendMessage(ctx.chat.id, message);
+};
+
+bot.start((ctx) => {
+  sendStandardMessage(ctx);
+
+  setInterval(() => sendStandardMessage(ctx), interval);
+});
+bot.on("message", (ctx) => {
+  const daysLeft = getDaysLeft();
+  let message = `Да заебали блядь! До отпуска осталось ${daysLeft} дней. Хуле не ясно?!`;
+
+  if (daysLeft <= 0) {
+    message = `Отпуск блеать! Иди купайся!`;
+  }
+
+  ctx.reply(message);
+});
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
